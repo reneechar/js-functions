a16939e3ebedc12f7461b5ce6a68d1fd63214ab1
Renee <rchar20@gmail.com>
2016-09-06 18:26:51 -1000

    isEven fx created

--- functions.js
+++ functions.js
@@ -171,7 +171,13 @@ function maximum(x,y) {
  * @param {number} n
  * @return {boolean} the number is even
  */
-
+function isEven(n) {
+	if (n%2 ===0) {
+		return true;
+	} else {
+		return false;
+	}
+}
 
 /**
  * Returns true if `n` is odd.
